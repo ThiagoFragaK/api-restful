f9c307deac51e1123106b35dbb1122b7a2375fd4
ThiagoFragaK <thiagofraga@proton.me>
2024-09-24 08:58:29 -0300

    Model files

---
+++ app/Http/Controllers/ManufacturerController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Manufacturer;
+use App\Http\Requests\StoreManufacturerRequest;
+use App\Http\Requests\UpdateManufacturerRequest;
+
+class ManufacturerController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(StoreManufacturerRequest $request)
+    {
+        //
+    }
+
+    public function edit(Manufacturer $manufacturer)
+    {
+        //
+    }
+
+    public function destroy(Manufacturer $manufacturer)
+    {
+        //
+    }
+}
