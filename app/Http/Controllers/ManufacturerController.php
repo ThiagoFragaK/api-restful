1741b4ca7722ac195eca3a9d41c45946b01b8d39
ThiagoFragaK <thiagofraga@proton.me>
2024-09-24 19:04:26 -0300

    Add: Model Controller Service

--- app/Http/Controllers/ManufacturerController.php
+++ app/Http/Controllers/ManufacturerController.php
@@ -2,29 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use App\Models\Manufacturer;
 use App\Http\Requests\StoreManufacturerRequest;
 use App\Http\Requests\UpdateManufacturerRequest;
+use App\Services\ManufacturersServices;
 
 class ManufacturerController extends Controller
 {
-    public function index()
+    private ManufacturersServices $ManufacturersServices;
+    public function __construct()
     {
-        //
+        $this->ManufacturersServices = new ManufacturersServices();
     }
 
-    public function store(StoreManufacturerRequest $request)
+    public function index(): JsonResponse
     {
-        //
+        return response()->json((object) [
+            "data" => $this->ManufacturersServices->getManufacturers(),
+            "status" => 200,
+        ]);
     }
 
-    public function edit(Manufacturer $manufacturer)
+    public function store(Request $request): JsonResponse
     {
-        //
+        return response()->json((object) [
+            "message" => "Manufacturer created successfully",
+            "data" => $this->ManufacturersServices->createManufacturer([
+                "name" => $request->get("name"),
+                "code" => $request->get("code"),
+            ]),
+            "status" => 201,
+        ]);
     }
 
-    public function destroy(Manufacturer $manufacturer)
+    public function edit(Int $id, Request $request): JsonResponse
     {
-        //
+        return response()->json((object) [
+            "message" => "Manufacturer edited successfully",
+            "data" => $this->ManufacturersServices->editManufacturer($id, [
+                "name" => $request->get("name"),
+                "code" => $request->get("code"),
+            ]),
+            "status" => 200,
+        ]);
+    }
+
+    public function destroy(Int $id): JsonResponse
+    {
+        return response()->json((object) [
+            "message" => "Manufacturer deleted successfully",
+            "data" => $this->ManufacturersServices->deleteManufacturer($id),
+            "status" => 204,
+        ]);
     }
 }
