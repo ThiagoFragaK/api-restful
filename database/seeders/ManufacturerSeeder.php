612022435e6fd32bc55841c9e884e61450b8f31f
ThiagoFragaK <thiagofraga@proton.me>
2024-09-24 19:09:24 -0300

    Add: Manufacturer seeds

--- database/seeders/ManufacturerSeeder.php
+++ database/seeders/ManufacturerSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Manufacturer;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,27 @@ class ManufacturerSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Manufacturer::insert([
+            [
+                "name" => "Renault",
+                "code" => "REN",
+                "status" => true,
+            ],
+            [
+                "name" => "BMW",
+                "code" => "BMW",
+                "status" => true,
+            ],
+            [
+                "name" => "Toyotta",
+                "code" => "TOT",
+                "status" => true,
+            ],
+            [
+                "name" => "Honda",
+                "code" => "HON",
+                "status" => true,
+            ],
+        ]);
     }
 }
