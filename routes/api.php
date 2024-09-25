ea3b6b00eea82f3c698bcd09fa9c1dcac47e813c
ThiagoFragaK <thiagofraga@proton.me>
2024-09-24 21:44:36 -0300

    add: fallback route

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ManufacturerController;
 
+Route::fallback(function () {
+    return response()->json((object) [
+        "message" => "This isn't the route you're looking for.",
+        "status" => 404,
+    ]);
+});
+
 Route::middleware('auth:api')->group(function () {
     Route::controller(ManufacturerController::class)
-        ->prefix('manufacturer')
+        ->prefix('/manufacturers')
         ->group(function () {
             Route::get('/', 'index');
             Route::post('/', 'store');
